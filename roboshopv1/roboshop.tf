3fe06aaae3abd7f24ebe76a2b78b96999190a9c1
Naresh kumar <naresh.kesa19@gmail.com>
2023-08-31 00:22:56 -0700

    Terraform

--- roboshopv1/roboshop.tf
+++ roboshopv1/roboshop.tf
@@ -7,6 +7,13 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 }
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z10341773SF2NEEPPHOLR"
+  name    = "frontend-dev.nkdevops74.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
 resource "aws_instance" "mongodb" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
@@ -15,6 +22,13 @@ resource "aws_instance" "mongodb" {
   tags = {
     Name = "mongodb"
   }
+}
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z10341773SF2NEEPPHOLR"
+  name    = "mongodb-dev.nkdevops74.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
 }
 resource "aws_instance" "catalogue" {
   ami           = "ami-03265a0778a880afb"
@@ -25,6 +39,13 @@ resource "aws_instance" "catalogue" {
     Name = "catalogue"
   }
 }
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z10341773SF2NEEPPHOLR"
+  name    = "catalogue-dev.nkdevops74.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
 resource "aws_instance" "redis" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
@@ -33,6 +54,13 @@ resource "aws_instance" "redis" {
   tags = {
     Name = "redis"
   }
+}
+resource "aws_route53_record" "redis" {
+  zone_id = "Z10341773SF2NEEPPHOLR"
+  name    = "redis-dev.nkdevops74.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
 }
 resource "aws_instance" "user" {
   ami           = "ami-03265a0778a880afb"
@@ -43,6 +71,13 @@ resource "aws_instance" "user" {
     Name = "user"
   }
 }
+resource "aws_route53_record" "user" {
+  zone_id = "Z10341773SF2NEEPPHOLR"
+  name    = "user-dev.nkdevops74.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
 resource "aws_instance" "cart" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
@@ -51,6 +86,13 @@ resource "aws_instance" "cart" {
   tags = {
     Name = "cart"
   }
+}
+resource "aws_route53_record" "cart" {
+  zone_id = "Z10341773SF2NEEPPHOLR"
+  name    = "cart-dev.nkdevops74.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
 }
 resource "aws_instance" "mysql" {
   ami           = "ami-03265a0778a880afb"
@@ -61,6 +103,13 @@ resource "aws_instance" "mysql" {
     Name = "mysql"
   }
 }
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z10341773SF2NEEPPHOLR"
+  name    = "mysql-dev.nkdevops74.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
 resource "aws_instance" "shipping" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
@@ -69,6 +118,13 @@ resource "aws_instance" "shipping" {
   tags = {
     Name = "shipping"
   }
+}
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z10341773SF2NEEPPHOLR"
+  name    = "shipping-dev.nkdevops74.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
 }
 resource "aws_instance" "rabbitmq" {
   ami           = "ami-03265a0778a880afb"
@@ -79,12 +135,26 @@ resource "aws_instance" "rabbitmq" {
     Name = "rabbitmq"
   }
 }
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z10341773SF2NEEPPHOLR"
+  name    = "rabbitmq-dev.nkdevops74.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
 resource "aws_instance" "payment" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.small"
   vpc_security_group_ids = ["sg-029ac5130dcdaed8c"]
 
   tags = {
     Name = "payment"
   }
 }
+  resource "aws_route53_record" "payment" {
+    zone_id = "Z10341773SF2NEEPPHOLR"
+    name    = "payment-dev.nkdevops74.online"
+    type    = "A"
+    ttl     = 30
+    records = [aws_instance.payment.private_ip]
+  }
